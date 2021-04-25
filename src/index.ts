795b3aefad6c877e09910719693911c48508bf1d
wesley <lelinho93@gmail.com>
2021-04-25 00:16:25 -0300

    server and database connection

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,44 @@
+import dotenv from 'dotenv'
+
 const express = require('express')
-import { Request, Response } from 'express'
+const mongoose = require('mongoose')
+const { MONGO_URI } = require('./config')
+const postsRoutes = require('../routes/api/router')
+
+
+//************************Config************************* */
 
 const app = express()
 
-app.get('/', (req: Request, res: Response) => {
-    res.send("Hello Node!")
+app.use(express.json())
+
+dotenv.config()
+
+const cors = require("cors")
+
+app.use((req: any, res: any, next: any) => {
+    res.header("Access-Control-Allow-Origin")
+    app.use(cors())
+    next()
 })
+
+
+//**********************DB Connection******************** */
+
+mongoose.connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log('MongoDB connected!'))
+.catch((error: any) => console.log(error))
+
+//********************** Routes **************************** */
+
+app.use(postsRoutes)
+
+
+//************************Server************************** */
 
 const PORT = process.env.PORT || 5000
 
-app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
+app.listen(PORT, () => console.log(`Server is running in http://localhost:${PORT}`))
